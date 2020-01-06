2463b6e57928b597ed672fff7a905861e5ae5c6e
SHUBHAM1512 <vshubham616@gmail.com>
2020-01-06 17:17:34 +0530

    Update views.py

--- essjob/views.py
+++ essjob/views.py
@@ -8,7 +8,7 @@ import requests
 import json
 from requests.auth import HTTPDigestAuth
 #import cx_Oracle
-
+import re
 
 def essjob(request):
 	'''
@@ -30,51 +30,29 @@ def essjob(request):
 	S5='' 
 
 
+	BusinessUnitIds = 300001096431127
+	BatchSourceIds  = 300001107407671
+	sysdate = 2019-12-27
+	fromDate = 'null'
+	ToDate = 'null'
+
+		
+
 	url="https://efip-dev1.fa.us6.oraclecloud.com:443/fscmService/ErpIntegrationService"
 	headers = {'content-type': 'text/xml'}
-	body = """<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
-	xmlns:typ="http://xmlns.oracle.com/apps/financials/commonModules/shared/model/erpIntegrationService/types/">
-	<soapenv:Header/>
-	<soapenv:Body>
-	   <typ:submitESSJobRequest>
-		  <typ:jobPackageName>/oracle/apps/ess/financials/receivables/transactions/autoInvoices/</typ:jobPackageName>
-		  <typ:jobDefinitionName>AutoInvoiceMasterEss</typ:jobDefinitionName>
-		  <!--Zero or more repetitions:-->
-			  <typ:paramList>1</typ:paramList>
-			 <typ:paramList>300001096431127</typ:paramList>
-			 <typ:paramList>300000311435966</typ:paramList>
-			 <typ:paramList>12/11/19</typ:paramList>
-			 <typ:paramList>null</typ:paramList>
-			 <typ:paramList>null</typ:paramList>
-			 <typ:paramList>null</typ:paramList>
-			 <typ:paramList>null</typ:paramList>
-			 <typ:paramList>null</typ:paramList>
-			 <typ:paramList>null</typ:paramList>
-			 <typ:paramList>null</typ:paramList>
-			 <typ:paramList>null</typ:paramList>
-			 <typ:paramList>null</typ:paramList>
-			 <typ:paramList>null</typ:paramList>
-			 <typ:paramList>null</typ:paramList>
-			 <typ:paramList>null</typ:paramList>
-			 <typ:paramList>null</typ:paramList>
-			 <typ:paramList>null</typ:paramList>
-			 <typ:paramList>null</typ:paramList>
-			 <typ:paramList>null</typ:paramList>
-			 <typ:paramList>null</typ:paramList>
-			 <typ:paramList>null</typ:paramList>
-			 <typ:paramList>null</typ:paramList>
-			 <typ:paramList>Y</typ:paramList>
-			 <typ:paramList>null</typ:paramList>
-	   </typ:submitESSJobRequest>
-	</soapenv:Body>
-	</soapenv:Envelope>"""
 
-
-	response = requests.post(url,data=body,headers=headers,auth=('oalo2c_ww@oracle.com', 'Owcw2!365'))
-	print(response)
-	print(response.content)
+	xmlfile = open('test.xml','r')
+	body = xmlfile.read()
+	response = requests.post(url, data=body.format(BusinessUnitIds=BusinessUnitIds, BatchSourceIds=BatchSourceIds, sysdate=sysdate,fromDate=fromDate,ToDate=ToDate), headers=headers,auth=('oalo2c_ww@oracle.com', 'Owcw2!365'))
 	
-	list13 = []
-	list13.append(S0)
+	my_string = response.content.decode('utf-8')
+	my_string_result_start_index = my_string.rfind('<result')
+	my_string_result_end_index = my_string.rfind('result>')
+	final_string = my_string[my_string_result_start_index : my_string_result_end_index]
+	final_output =  re.findall(r'\d+', final_string)
 	
-	return HttpResponse(response)
+	final_keys=['Job_ID']
+	combo=zip(final_keys,final_output)
+	final_result=json.dumps(dict(combo))
+	
+	return HttpResponse(final_result)
